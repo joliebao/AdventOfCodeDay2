145dc4e89b2bea40e183c4a53737ca2d5307b5dd
joliebao <baojolie@gmail.com>
2025-03-09 00:27:24 -0500

    Getting closer...

--- AdventDay.java
+++ AdventDay.java
@@ -5,8 +5,6 @@ import java.util.Arrays;
 public class AdventDay {
     private String[] list = new String[1000];
     private int passes;
-    private int decrNum;
-    private int incrNum;
 
     public AdventDay(String[] array) {
         list = array;
@@ -50,20 +48,15 @@ public class AdventDay {
         return passes; //number of lines that pass
     }
 
-    private void part2Counter(String[] list){
-        decrNum = 0;
-        incrNum = 0;
+    private ArrayList<Integer> getDifferences(String[] arr){
+        ArrayList<Integer> differences = new ArrayList<>();
+        for (int i = 0; i < arr.length - 1; i ++){
+            int prev = Integer.parseInt(arr[i]);
+            int curr = Integer.parseInt(arr[i+1]);
 
-        for (int i = 0; i < list.length - 1; i++) {
-            int prev = Integer.parseInt(list[i]);
-            int curr = Integer.parseInt(list[i+1]);
-
-            if (prev < curr){
-                incrNum ++;
-            } else if (prev > curr){
-                decrNum ++;
-            }
+            differences.add(prev-curr);
         }
+        return differences;
     }
 
     // 378 -- need a way to remove prev and determine when to instead of curr
@@ -71,81 +64,30 @@ public class AdventDay {
         passes = 0;
         int i2 = 1;
         for (String line : list) {
+            int badLevels = 0;
+
             System.out.println();
             System.out.println("(" + i2 + ")  ______");
             i2++;
-            boolean incr = false;
-            boolean decr = false;
-            boolean safe = true;
 
             String[] lineElements = line.split(" ");
+            ArrayList<Integer> lineDiff = getDifferences(lineElements);
 
-            part2Counter(lineElements);
+            System.out.println(lineDiff);
 
-            if (decrNum > incrNum){
-                decr = true;
-            } else if (incrNum > decrNum){
-                incr = true;
-            } else {
-                safe = false;
-            }
-
-            ArrayList<String> removalList = new ArrayList<String>();
-            for (String num : lineElements){
-                removalList.add(num);
-            }
-            removalList.add("0"); // to check the last index too
-
-            // extra case; need condition so that values aren't removed if they are correct but still needs to consider removing the first or later indexes if they're wrong
-            for (int i = 0; i < removalList.size() - 1; i++){
-                System.out.println(removalList);
-                int prev = Integer.parseInt(removalList.get(i));
-                System.out.println(prev);
-                int curr = Integer.parseInt(removalList.get(i+1));
-
-                if (prev > curr){ // if i is greater than i+1
-                    if (incr) {
-                        int next = Integer.parseInt(removalList.get(i+1));
-                        if (Math.abs(prev - next) < 3) {
-                            removalList.remove(i + 1);
-                            i--;
-                            System.out.println("working");
-                        } else if (Math.abs(curr - prev) > 3) {
-                            removalList.remove(i);
-                            if (i == 1){
-                                i = 0;
-                            }
-                            i--;
-                        }
-                    }
-                } else if (prev < curr){ // if i is less than i+1
-                    int next = Integer.parseInt(removalList.get(i+1));
-                    if (Math.abs(prev - next) < 3) {
-                        removalList.remove(i + 1);
-                        i--;
-                    } else if (Math.abs(curr - prev) > 3) {
-                        removalList.remove(i);
-                        if (i == 1){
-                            i = 0;
-                        }
-                        i--;
-                    }
-                } else {
-                    removalList.remove(i+1);
-                    i--;
+            for (int i = 0; i < lineDiff.size() - 1; i++){
+                if (Math.abs(lineDiff.get(i)) > 3){
+                    badLevels ++;
+                } else if (lineDiff.get(i) == 0){
+                    badLevels ++;
+                } else if (lineDiff.get(i) > 0 && lineDiff.get(i+1) < 0){
+                    badLevels ++;
+                } else if (lineDiff.get(i) < 0 && lineDiff.get(i+1) > 0) {
+                    badLevels++;
                 }
-
-//                if (Math.abs(curr - prev) > 3){
-//                    removalList.remove(i+1);
-//                    i--;
-//                }
-
-                if (removalList.size() < lineElements.length - 1){
-                    safe = false;
-                }
-
             }
-            if (safe) {
+
+            if (badLevels <= 1) {
                 passes++;
                 System.out.println("PASS");
             }
